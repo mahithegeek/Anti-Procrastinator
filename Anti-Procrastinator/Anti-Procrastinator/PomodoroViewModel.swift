f69185f418101684a8ed2ea6d17e58162b98e354
Mahendra Nimishakavi <Mahendra.Nimishakavi@ca.com>
2018-11-23 12:45:39 +0530

    Initial project structure and basic timer functionality

---
+++ Anti-Procrastinator/Anti-Procrastinator/PomodoroViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  PomodoroViewModel.swift
+//  Anti-Procrastinator
+//
+//  Created by nimma01 on 23/11/18.
+//  Copyright © 2018 nimma01. All rights reserved.
+//
+
+import UIKit
+
+class PomodoroViewModel {
+
+    
+    
+}
